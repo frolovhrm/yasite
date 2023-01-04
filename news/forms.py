90262c0a66d66904c6091fc8eee13f172cd3b636
frolovhrm <euromen@yandex.ru>
2023-01-05 01:55:18 +0300

    #23

---
+++ news/forms.py
@@ -0,0 +1,10 @@
+from django import forms
+
+from .models import Category
+
+
+class NewsForm(forms.Form):
+    title = forms.CharField(max_length=150, label='Название', widget=forms.TextInput(attrs={"class": "form-control"}))
+    content = forms.CharField(label='Текст', required=False, widget=forms.Textarea(attrs={"class": "form-control", "rows": 5}))
+    is_publishes = forms.BooleanField(label='Опубликовано?', initial=True)
+    category = forms.ModelChoiceField(empty_label='Выбирите категорию', queryset=Category.objects.all(), label='Категория', widget=forms.Select(attrs={"class": "form-control"}))
