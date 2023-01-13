de952e960993736c327e8f03c1a6213ee6741437
frolovhrm <euromen@yandex.ru>
2023-01-13 12:21:58 +0300

    #29 Класс DetailView

--- news/views.py
+++ news/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_object_or_404, redirect
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 
 from .models import News, Category
 from .forms import NewsForm
@@ -20,6 +20,12 @@ class HomeNews(ListView):
     def get_queryset(self):
         return News.objects.filter(is_publishes=True)
 
+
+class ViewNews(DetailView):
+    model = News
+    context_object_name = "news_item"
+    # pk_url_kwarg = 'news_id'
+    # template_name = 'news/news_detail.html'
 
 # def index(request):
 #     news = News.objects.all()
@@ -51,10 +57,10 @@ def get_category(request, category_id):
     return render(request, 'news/category.html', {'news': news, 'category': category})
 
 
-def view_news(request, news_id):
-    # news_item = News.objects.get(pk=news_id)
-    news_item = get_object_or_404(News, pk=news_id)
-    return render(request, 'news/view_news.html', {"news_item": news_item})
+# def view_news(request, news_id):
+#     # news_item = News.objects.get(pk=news_id)
+#     news_item = get_object_or_404(News, pk=news_id)
+#     return render(request, 'news/view_news.html', {"news_item": news_item})
 
 
 def add_news(request):
