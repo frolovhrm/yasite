aa34bbe6aada24028f3747add7dca3eba487f43c
frolovhrm <euromen@yandex.ru>
2023-01-18 16:27:11 +0300

    #36 ORM part 5

--- news/models.py
+++ news/models.py
@@ -12,6 +12,7 @@ class News(models.Model):
     photo = models.ImageField(upload_to='photos/%Y/%m/%d', verbose_name='Фото', blank=True)
     is_publishes = models.BooleanField(default=True, verbose_name='Размещена')
     category = models.ForeignKey('Category', on_delete=models.PROTECT, verbose_name='Категория') #  null=True,
+    views = models.IntegerField(default=0)
 
     def get_absolute_url(self):
         return reverse('view_news', kwargs={'pk': self.pk})
