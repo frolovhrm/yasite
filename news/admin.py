1963c8312636b23e82a7fa0ff21758d5ef045fe7
frolovhrm <euromen@yandex.ru>
2023-01-26 22:07:58 +0300

    #41 Кастомизация админки

--- news/admin.py
+++ news/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
 
 from .models import News, Category
 
@@ -6,12 +7,23 @@ from .models import News, Category
 # Register your models here.
 
 class NewsAdmin(admin.ModelAdmin):
-    list_display = ('id', 'title', 'category', 'created_at', 'is_publishes')
+    list_display = ('id', 'title', 'category', 'created_at', 'is_publishes', 'get_photo')
     list_display_links = ('id', 'title')
     search_fields = ('title', 'content')
     list_editable = ('is_publishes',)
     list_filter = ('is_publishes',  'category')
+    filds = ('title', 'category', 'content', 'photo', 'get_photo', 'is_publishes', 'views',  'created_at', 'updated_at')
+    readonly_fields = ('get_photo', 'views',  'created_at', 'updated_at')
 
+    def get_photo(self, obj):
+        if obj.photo:
+            return mark_safe(f'<img src="{obj.photo.url}" width="75">')
+        else:
+            return  'Фото не установлено'
+
+    get_photo.short_description = 'Миниатюра'
+
+    save_on_top = True
 
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ('id', 'title',)
@@ -21,3 +33,6 @@ class CategoryAdmin(admin.ModelAdmin):
 
 admin.site.register(News, NewsAdmin)
 admin.site.register(Category, CategoryAdmin)
+
+admin.site.site_title = 'Управление новостями'
+admin.site.site_header = 'Управление новостями'
