0bf3f614906612489f1c53a06fe26b917ccee50c
frolovhrm <euromen@yandex.ru>
2023-02-03 18:36:39 +0300

    #45 Регистрация ч.1

--- news/views.py
+++ news/views.py
@@ -8,6 +8,27 @@ from .models import News, Category
 from .forms import NewsForm
 from .utils import MyMixin
 
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Вы успешно зарегистрировались')
+            return redirect('login')
+        else:
+            messages.error(request, 'Ошибка регистрации')
+    else:
+        form = UserCreationForm()
+    return render(request, 'news/register.html', {'form': form})
+
+
+def login(request):
+    return render(request, 'news/login.html')
+
 
 def test(request):
     objects = ['aaaa1', 'aaaa2', 'aaaa3', 'aaaa4', 'aaaa5', 'aaaa6', 'aaaa7']
@@ -84,7 +105,7 @@ class CreateNews(LoginRequiredMixin, CreateView):
     form_class = NewsForm
     template_name = 'news/add_news.html'
     # success_url = reverse_lazy('home') # Будет работать get_absolute_url
-    login_url = '/admin/'   # если не авторизован на страницу авторизации
+    login_url = '/admin/'  # если не авторизован на страницу авторизации
     # raise_exception = True  # - в этом случае ошибка доступа
 
 # def add_news(request):
