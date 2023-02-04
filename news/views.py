43e850f45898cdf8000a0792a9b5a9c5aad2933b
frolovhrm <euromen@yandex.ru>
2023-02-04 19:44:26 +0300

    #47 Авторизация

--- news/views.py
+++ news/views.py
@@ -5,29 +5,45 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.core.paginator import Paginator
 
 from .models import News, Category
-from .forms import NewsForm, UserRegisterForm
+from .forms import NewsForm, UserRegisterForm, UserLoginForm
 from .utils import MyMixin
+from django.contrib.auth import login, logout
 
 # from django.contrib.auth.forms import UserCreationForm - форма переопределена в forms.py в UserRegisterForm
+
 from django.contrib import messages
 
 
 def register(request):
     if request.method == 'POST':
-        form = UserRegisterForm(request.POST)   # используется подменная форма
+        form = UserRegisterForm(request.POST)  # используется подменная форма
         if form.is_valid():
-            form.save()
+            user = form.save()
+            login(request, user)
             messages.success(request, 'Вы успешно зарегистрировались')
-            return redirect('login')
+            return redirect('home')
         else:
             messages.error(request, 'Ошибка регистрации')
     else:
-        form = UserRegisterForm()   # используется подменная форма
+        form = UserRegisterForm()  # используется подменная форма
     return render(request, 'news/register.html', {'form': form})
 
 
-def login(request):
-    return render(request, 'news/login.html')
+def user_login(request):
+    if request.method == 'POST':
+        form = UserLoginForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('home')
+    else:
+        form = UserLoginForm()
+    return render(request, 'news/login.html', {"form": form})
+
+
+def user_logout(request):
+    logout(request)
+    return redirect('login')
 
 
 def test(request):
